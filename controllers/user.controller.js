2b4dbb0e3a6e65890f258c41ed0ebee7406de213
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2025-06-02 20:57:44 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
 const UserModel = require("../models/user.model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const RegisterUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
